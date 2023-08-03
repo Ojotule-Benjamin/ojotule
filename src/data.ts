5e45b1b49d45e2864c5cf76549c88d80d0534dc2
Ojotule Benjamin Ajodo <benjamin.ojotule.ajodo@gmail.com>
2023-08-03 09:37:43 +0100

    made some changes

--- src/data.ts
+++ src/data.ts
@@ -2,37 +2,7 @@ export const featuredPortfolio = [
   {
     id: 1,
     title: "Ritz Urban Luxury",
-    img: require("./assets/ritzhub.png"),
-  },
-  {
-    id: 2,
-    title: "Dsarah Daniels website",
-    img: require("./assets/dsarahdaniels.png"),
-    link: "https://dsarah-daniels.vercel.app",
-  },
-];
-
-export const webPortfolio = [
-  {
-    id: 1,
-    title: "Dsarah Daniels Website",
-    img: require("./assets/dsarahdaniels.png"),
-    link: "https://dsarah-daniels.vercel.app",
-  },
-  {
-    id: 2,
-    title: "Ecocyntec",
-    img: require("./assets/ecocyntec.png"),
-    link: "https://ecocyntec.vercel.app",
-    desc: `ECOCYNTEC Limited, a leading provider of professional engineering services and energy solutions for the upstream, midstream, and downstream sectors of the oil and energy industry. `,
-  },
-];
-
-export const mobilePortfolio = [
-  {
-    id: 1,
-    title: "Ritz Hub",
-    img: require("./assets/ritzhub.png"),
+    img: require("./assets/ritzHub1.png"),
     desc: `Ritz offers a convenient transportation solution where users
     have the option to either request a ride from a pool of
     licensed drivers or rent a vehicle for a specific duration.
@@ -42,8 +12,61 @@ export const mobilePortfolio = [
   },
   {
     id: 2,
-    title: "Ritz Hub",
-    img: require("./assets/ritzhub2.png"),
+    title: "Dsarah Daniels website",
+    img: require("./assets/dsarahdaniels.png"),
+    link: "https://dsarah-daniels.vercel.app",
+    desc: `DsarahDaniels is an event management company that specializes in 
+    handling all types of events and offers a delightful selection of wedding accessories. 
+    With our expertise in event planning and attention to detail, we transform your vision into reality, 
+    ensuring a seamless and unforgettable experience for your special occasions.`,
+  },
+  {
+    id: 2,
+    title: "Ecocyntec",
+    img: require("./assets/ecocyntec.png"),
+    link: "https://ecocyntec.vercel.app",
+    desc: `ECOCYNTEC Limited, a leading provider of professional engineering services and 
+    energy solutions for the upstream, midstream, and downstream sectors of the oil and energy industry. `,
+  },
+];
+
+export const webPortfolio = [
+  {
+    id: 1,
+    title: "Dsarah Daniels Website",
+    img: require("./assets/dsarahdaniels.png"),
+    link: "https://dsarah-daniels.vercel.app",
+    desc: `DsarahDaniels is an event management company that specializes in handling all types of 
+    events and offers a delightful selection of wedding accessories. With our expertise in event planning 
+    and attention to detail, we transform your vision into reality, ensuring a seamless and unforgettable experience 
+    for your special occasions.`,
+  },
+  {
+    id: 2,
+    title: "Ecocyntec",
+    img: require("./assets/ecocyntec.png"),
+    link: "https://ecocyntec.vercel.app",
+    desc: `ECOCYNTEC Limited, a leading provider of professional engineering services and 
+    energy solutions for the upstream, midstream, and downstream sectors of the oil and energy industry. `,
+  },
+];
+
+export const mobilePortfolio = [
+  {
+    id: 1,
+    title: "Ritz Urban Luxury",
+    img: require("./assets/ritzHub1.png"),
+    desc: `Ritz offers a convenient transportation solution where users
+    have the option to either request a ride from a pool of
+    licensed drivers or rent a vehicle for a specific duration.
+    The project is currently in the development phase, and we are
+    actively working on building the client, driver, and admin
+    dashboard applications.`,
+  },
+  {
+    id: 2,
+    title: "Ritz Urban Luxury",
+    img: require("./assets/ritzHub2.png"),
     desc: `Ritz offers a convenient transportation solution where users
     have the option to either request a ride from a pool of
     licensed drivers or rent a vehicle for a specific duration.
