7011c488441e8f46235c87437fff6a3be5f9c864
Ojotule Benjamin Ajodo <benjamin.ojotule.ajodo@gmail.com>
2023-08-03 09:56:50 +0100

    fixed the changes

--- src/data.ts
+++ src/data.ts
@@ -15,18 +15,20 @@ export const featuredPortfolio = [
     title: "Dsarah Daniels website",
     img: require("./assets/dsarahdaniels.png"),
     link: "https://dsarah-daniels.vercel.app",
-    desc: `DsarahDaniels is an event management company that specializes in 
-    handling all types of events and offers a delightful selection of wedding accessories. 
-    With our expertise in event planning and attention to detail, we transform your vision into reality, 
-    ensuring a seamless and unforgettable experience for your special occasions.`,
+    desc: ` DsarahDaniels is an event management company that 
+    specializes in handling all types of events and offers a delightful selection of 
+    wedding accessories. With our expertise in event planning and attention to detail, 
+    we transform your vision into reality, ensuring a seamless 
+    and unforgettable experience for your special occasions.`,
   },
   {
-    id: 2,
+    id: 3,
     title: "Ecocyntec",
     img: require("./assets/ecocyntec.png"),
     link: "https://ecocyntec.vercel.app",
     desc: `ECOCYNTEC Limited, a leading provider of professional engineering services and 
-    energy solutions for the upstream, midstream, and downstream sectors of the oil and energy industry. `,
+    energy solutions for the upstream, midstream, and downstream sectors of the oil 
+    and energy industry. `,
   },
 ];
 
@@ -36,10 +38,11 @@ export const webPortfolio = [
     title: "Dsarah Daniels Website",
     img: require("./assets/dsarahdaniels.png"),
     link: "https://dsarah-daniels.vercel.app",
-    desc: `DsarahDaniels is an event management company that specializes in handling all types of 
-    events and offers a delightful selection of wedding accessories. With our expertise in event planning 
-    and attention to detail, we transform your vision into reality, ensuring a seamless and unforgettable experience 
-    for your special occasions.`,
+    desc: ` DsarahDaniels is an event management company that 
+    specializes in handling all types of events and offers a delightful selection of 
+    wedding accessories. With our expertise in event planning and attention to detail, 
+    we transform your vision into reality, ensuring a seamless 
+    and unforgettable experience for your special occasions.`,
   },
   {
     id: 2,
@@ -47,7 +50,8 @@ export const webPortfolio = [
     img: require("./assets/ecocyntec.png"),
     link: "https://ecocyntec.vercel.app",
     desc: `ECOCYNTEC Limited, a leading provider of professional engineering services and 
-    energy solutions for the upstream, midstream, and downstream sectors of the oil and energy industry. `,
+    energy solutions for the upstream, midstream, and downstream sectors of the oil 
+    and energy industry. `,
   },
 ];
 
@@ -66,7 +70,7 @@ export const mobilePortfolio = [
   {
     id: 2,
     title: "Ritz Urban Luxury",
-    img: require("./assets/ritzHub2.png"),
+    img: require("./assets/ritzHub1.png"),
     desc: `Ritz offers a convenient transportation solution where users
     have the option to either request a ride from a pool of
     licensed drivers or rent a vehicle for a specific duration.
